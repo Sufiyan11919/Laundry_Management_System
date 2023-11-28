5809975ccadbf825364e846352076e0c8e057625
sufi <sufiyanshaikh11919@gmail.com>
2023-11-28 01:10:21 -0800

    created new project in spring

---
+++ server/src/main/java/com/laundry_management_system/fullstack/web/app/FullStackWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.laundry_management_system.fullstack.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackWebAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/laundry_management_system/fullstack/web/app/FullStackWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laundry_management_system.fullstack.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
